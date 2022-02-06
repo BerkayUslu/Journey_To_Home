be6114ab2c618021c00576d0691b23dd015ef6d2
BerkayUslu <mberkayuslu@outlook.com>
2022-02-06 14:41:49 +0300

    up

---
+++ Journey to Home/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //States
+    string IDLING = "IDLING";
+    string SKIPPING = "SKIPPING";
+    string CLIMBING = "CLIMBING";
+    string FALLING = "FALLING";
+    string DUCKING = "DUCKING";
+    string JUMPING = "JUMPING";
+    string HURT = "HURT";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
